d092514b7f4d9015931a205f4a96bb69228f8f5f
Adrian42X <89139357+Adrian42X@users.noreply.github.com>
2023-10-16 22:52:42 +0300

    Initial commit

---
+++ Data/AppService.cs
@@ -0,0 +1,42 @@
+﻿using QuizzProject.Models;
+
+namespace QuizWithBlazor.Data
+{
+    public static class AppService
+    {
+        private static QuizService service = new QuizService();
+        public static int index = 0;
+        public static void Start(string difficulty,string playerName)
+        {
+            service.playerName = playerName;
+            service.selectedDifficulty = difficulty;
+            service.StartQuizz();
+        }
+
+        public static Question GetCurrentQuestion()
+        {
+            return service.GetQuestion(index);
+        }
+
+        public static void CheckAnswers(List<bool> answers)
+        {
+            service.CheckQuestionAnswer(index, answers);
+        }
+
+        public static void AddStats()
+        {
+            service.AddPlayerStats();
+        }
+
+        public static void NewQuiz()
+        {
+            index = 0;
+            service.NewQuiz();
+        }
+
+        public static int Score => service.playerScore;
+        public static string PlayerName => service.playerName;
+        public static string Difficulty => service.selectedDifficulty;
+        public static List<Player> Players=>service.GetAllPlayers();
+    }
+}
